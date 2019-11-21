bfef997b59f480f9b7815939b7f854aa16b4a377
Matthew Deakos <mwoolnerdeakos@gmail.com>
2019-11-21 18:56:03 -0400

    Refactored into build_baseline func

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,4 @@
 import tensorflow as tf
-from utils import load_train, load_test
-from pipelines import *
 
 # Aliasing instead of importing. This is just for dev
 # since TF 2.0 has broken autocomplete basically.
@@ -8,29 +6,31 @@ from pipelines import *
 keras = tf.keras
 Conv2D = tf.keras.layers.Conv2D
 Dense = tf.keras.layers.Dense
-selu = tf.keras.activations.selu
+Flatten = tf.keras.layers.Flatten
+Dropout = tf.keras.layers.Dropout
 
-# Baseline Model
-images, labels = load_train()
-val_images, val_labels = load_test()
-dataset = make_train_pipeline(images, labels)
-validation = make_validation_pipeline(val_images, val_labels)
-input_shape = images.shape[1:]
 
 # Hyperparameters
 N_DENSE_UNITS = 128
 N_OUTPUTS = 10
 kernel_size = 3
 
-input_layer = keras.layers.Input(shape=input_shape, name="Input")
-model = selu(Conv2D(64, kernel_size, padding="same")(input_layer))
-model = selu(Conv2D(64, kernel_size, padding="same")(model))
-model = selu(Conv2D(64, kernel_size, padding="same")(model))
-outputs = Dense(N_OUTPUTS, activation="softmax")(model)
+# Model Design
+def build_baseline(input_shape):
+    input_layer = keras.layers.Input(shape=input_shape, name="Input")
+    conv1 = Conv2D(64, kernel_size, padding="same", activation=tf.nn.selu)(input_layer)
+    conv2 = Conv2D(64, kernel_size, padding="same", activation=tf.nn.selu)(conv1)
+    conv3 = Conv2D(64, kernel_size, padding="same", activation=tf.nn.selu)(conv2)
+    flattened = Flatten()(conv3)
+    dense1 = Dense(N_DENSE_UNITS, activation="selu")(flattened)
+    dropout = Dropout(0.5)(dense1)
+    outputs = Dense(N_OUTPUTS, activation="softmax")(dropout)
 
-model = tf.keras.Model(inputs=[input_layer], outputs=[outputs])
+    model = tf.keras.Model(inputs=[input_layer], outputs=[outputs])
 
-model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+    optimizer = tf.optimizers.Adam(lr=0.00001)
+    model.compile(
+        optimizer=optimizer, loss="categorical_crossentropy", metrics=["accuracy"]
+    )
 
-
-model.fit_generator(dataset,
+    return model
