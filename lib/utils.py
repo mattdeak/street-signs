b6644d02b18e1d18e46ba895b6e513b3ac4ea6a2
Matthew Deakos <mwoolnerdeakos@gmail.com>
2019-11-22 11:56:45 -0400

    Can load extra images from a npy file

--- lib/utils.py
+++ lib/utils.py
@@ -1,10 +1,16 @@
+import logging
 import numpy as np
 import scipy.io
 
 # Load format 2
 FORMAT2_TRAIN = "/home/matt/Projects/CV/final/data/raw/format2/train_32x32.mat"
 FORMAT2_TEST = "/home/matt/Projects/CV/final/data/raw/format2/test_32x32.mat"
-FORMAT2_EXTRA = "/home/matt/Projects/CV/final/data/raw/format2/extra_32x32.mat"
+FORMAT2_EXTRA_IMAGES = (
+    "/home/matt/Projects/CV/final/data/raw/format2/extra_images_32x32.npy"
+)
+FORMAT2_EXTRA_LABELS = (
+    "/home/matt/Projects/CV/final/data/raw/format2/extra_labels_32x32.npy"
+)
 
 
 def load_train(use_extra=False):
@@ -15,12 +21,15 @@ def load_train(use_extra=False):
     images = np.moveaxis(images, -1, 0)
 
     if use_extra:
-        extra = scipy.io.loadmat(FORMAT2_EXTRA)
-        extra_images = extra["X"]
-        extra_labels = extra["y"]
+        extra_images = np.load(FORMAT2_EXTRA_IMAGES)
+        extra_labels = np.load(FORMAT2_EXTRA_LABELS)
         extra_images = np.moveaxis(extra_images, -1, 0)
         images = np.vstack([images, extra_images])
         labels = np.vstack([labels, extra_labels])
+        # Randomly shuffle them together
+        idx = np.random.permutation(np.arange(images.shape[0]))
+        images = images[idx, :, :, :]
+        labels = labels[idx]
 
     return images, labels
 
